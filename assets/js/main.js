699d32bbe94168014d7869036c41d77a70dc32f5
Ariel Dorani <ariel@ladesign.co.il>
2016-11-23 20:04:47 +0200

    Ariel

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,56 @@ $('.cyb-button').click(function(){
 
 });
 
+$( document ).ready(function() {
+
+});
+
+$( "#kaftor" ).click(function() {
+
+
+    $( ".character2" ).css( "display", "block" );
+
+    $( ".character1" ).css( "display", "block" );
+    $( ".character1" ).addClass("animated-slow").addClass("slideFromRight ");
+
+    setTimeout(function(){
+        smalltoBig('.character2', 313, 439, 5, 10, 1500);
+    }, 1000);
+
+    setTimeout(function(){
+
+        $( ".safe" ).css( "display", "block" );
+        $( ".safe" ).addClass("animated-slow").addClass("bounceInDown ");
+        $( ".safe2" ).css( "display", "block" );
+        $( ".safe2" ).addClass("animated-slow").addClass("bounceInDown ");
+
+    }, 2000);
+
+    setTimeout(function(){
+
+        $( ".safe-shadow" ).css( "display", "block" );
+        fadeIn(".safe-shadow", 2000)
+
+    }, 2000);
+
+});
+
+function fadeIn(nameClass,time){
+    $(nameClass).animate({
+        'opacity': 1
+    }, time);
+}
+
+function smalltoBig(nameClass,x,y,bottom,left,time){
+    $(nameClass).animate({
+        'width': x,
+        'height': y,
+        bottom: bottom,
+        'margin-left': left
+    }, time);
+}
+
+
 function load(show) {
     if (show) {
         document.getElementById('fade').style.display='block';
