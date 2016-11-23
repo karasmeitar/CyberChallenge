4348cd6e43371f0de5cd13a706294b9d4962655e
karasmeitar <karasmeitar@gmail.com>
2016-11-23 13:43:19 +0200

    add js files

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$('.cyb-button').click(function(){
+    $('.cyb-main-background').addClass('slideout');
+    setTimeout(function() {
+        $('.cyb-main-background').css('display', 'none');
+    }, 5000 );
+    $('.cyb-second-background').css('display','block');
+    $('.cyb-second-background').addClass('slidein');
+
+})
