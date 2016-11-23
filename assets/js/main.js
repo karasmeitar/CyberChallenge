ee98420366b436cc2357f0f960cc00ad0b5f9d54
karasmeitar <karasmeitar@gmail.com>
2016-11-23 16:59:02 +0200

    css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
 $('.cyb-button').click(function(){
-    $('.cyb-main-background').addClass('slideout');
+   /* $('.cyb-main-background').addClass('slideout');*/
     //setTimeout(function() {
     //    $('.cyb-main-background').css('display', 'none');
     //}, 5000 );
     //$('.cyb-second-background').css('display','block');
     //$('.cyb-second-background').addClass('slidein');
-
+    changePosition('.cyb-main-background','-1637','768px',3000);
+    /*$('.cyb-main-background').animate({
+        'position-x': '-1637px',
+        'position-y': '768'
+    }, 3000, 'linear');*/
+    ('.cyb-tree').Hide();
 });
+
+
+function changePosition(nameClass,x,y,time){
+    $(nameClass).animate({
+        'background-position-x': x,
+        'background-position-y': y
+    }, time);
+}
 
 function load(show) {
     if (show) {
