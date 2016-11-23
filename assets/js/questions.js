c3ac00ec6d7038cf441e9a45b858308f3745e8f4
Nirpopik <nirpopik@gmail.com>
2016-11-23 20:15:49 +0200

    fix conflicts

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var questionManager = (function(){
 	var managerToReturn = {};
 	
@@ -90,55 +89,24 @@ var questionManager = (function(){
 	return managerToReturn;
 }());
 
-=======
-function buildQuestion() {
-    var field = document.getElementById("questionField");
-    var question = new manager.getRandomQuestion();
-    var legend = document.createElement("legend");
-    legend.innerHTML = question.questionContent;
-    field.appendChild(legend);
-    for (var answerKey in PosibleAnswers) {
-        var p = document.createElement("p");
-        p.answer = answerKey;
-        p.innerHTML = PosibleAnswers[answerKey].answerContent;
-        field.appendChild(p);
-    }
-}
 
-function runCode() {
-    var resultElement = document.getElementById('resultText');
-    resultElement.innerText = "";
+// function runCode() {
+//     var resultElement = document.getElementById('resultText');
+//     resultElement.innerText = "";
 
-    var codeText = document.getElementById('codeText').value;
-    codeText = "(function() {" + codeText + "}())";
-    try {
+//     var codeText = document.getElementById('codeText').value;
+//     codeText = "(function() {" + codeText + "}())";
+//     try {
 
-        var codeResult = eval(codeText);
-        if (codeResult == 2) {
-            resultElement.innerText = "Good Answer";
-        }
-        else {
-            resultElement.innerText = "Wrong Answer";
-        }
-    }
-    catch(err) {
-        resultElement.innerText = "Wrong Answer";
-    }
-}
-
-
-
-function buildQuestion() {
-	var field = document.getElementById("questionField");
-	var question = manager.getRandomQuestion();
-	var legend = document.createElement("legend");
-	legend.innerHTML = question.questionContent;
-	field.appendChild(legend);
-	for (var answerKey in PosibleAnswers) {
-		var p = document.createElement("p");
-		p.answer = answerKey;
-		p.innerHTML = PosibleAnswers[answerKey].answerContent;
-		field.appendChild(p);
-	}
-};
->>>>>>> ed33f6f6d27919c3d3293c98a8d982db95e54cb1
+//         var codeResult = eval(codeText);
+//         if (codeResult == 2) {
+//             resultElement.innerText = "Good Answer";
+//         }
+//         else {
+//             resultElement.innerText = "Wrong Answer";
+//         }
+//     }
+//     catch(err) {
+//         resultElement.innerText = "Wrong Answer";
+//     }
+// }
